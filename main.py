1a3b367788d472e3f080692c5249345b48e02aac
Jeremy Jany <jeremy.jany@exxcellent.de>
2023-11-05 16:10:51 +0100

    feat(*): adapt search params and remove loop

--- main.py
+++ main.py
@@ -5,43 +5,23 @@ import playsound
 mp3_file = "alarm.wav"
 
 
-def mine(word):
-    found = False
-    for i in range(3):
-        if found is False:
-            try:
-                fp = urllib.request.urlopen("https://www.lttstore.com/collections/all?page=" + str(i+1))
-                mybytes = fp.read()
-                mystr = mybytes.decode("utf8")
-                fp.close()
+def run(word, url):
+    try:
+        fp = urllib.request.urlopen(url)
+        mybytes = fp.read()
+        mystr = mybytes.decode("utf8")
+        fp.close()
 
-                found = word in mystr
-                print("\tSearch #" + str(i+1) + ": Finished.")
-            except Exception as e:
-                print("\tSearch #" + str(i+1) + ": Could not establish a connection: " + e.__str__())
+        if (word in mystr):
+            print("Go get some " + word)
+            playsound.playsound(mp3_file, True)
         else:
-            return found
-        time.sleep(1)
-    return found
+            print("No " + word + " today")
+            time.sleep(2)
 
-
-def run(word_to_search):
-    word_3080_found = mine(word_to_search)
-    counter = 1
-    while word_3080_found is False:
-        print(str(counter) + ": Not found. Wait 1 sec and try again...")
-        time.sleep(1)
-        word_3080_found = mine(word_to_search)
-        counter += 1
-
-    print(str(counter) + ": FOUND!!! GO GO GO!")
-
-    f = open("log.log", "a")
-    f.write("Tries done and three times seconds elapsed: " + str(counter) + "\n\n")
-    f.close()
-
-    playsound.playsound(mp3_file, True)
+    except Exception as e:
+        print("Could not establish a connection: " + e.__str__())
 
 
 if __name__ == '__main__':
-    run("3080")
+    run("Graupenrisotto", "https://www.ahoch2-suppenbar.de/")
